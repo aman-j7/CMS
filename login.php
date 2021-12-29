17a2af4f79378fafaaeeb36beb55467218cabbee
SHADOW-AJ <463amanj@gmail.com>
2021-12-29 12:03:18 +0530

    login update

--- login.php
+++ login.php
@@ -21,7 +21,7 @@ if(isset($_POST["submit"])){
 ?>
 <html>
 <head>
-<link rel="stylesheet" href="css/cms.css">
+<link rel="stylesheet" href="CSS/cms.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>E-Learning</title>
 </head>
@@ -49,11 +49,11 @@ if(isset($_POST["submit"])){
                   </div>
 
                   <div class="form-outline mb-4">
-                    <input type="password" name="password" required id="login2" class="form-control" />
+                    <input type="password" name="password" required id="login2" class="form-control" placeholder="Password"/>
                   </div>
 
                   <div class="text-center pt-1 mb-5 pb-1">
-                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit">Log in</button>
+                    <input class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit" name="submit" value="Log In"/>
                     <br>
                     <a class="text-muted" href="forget.php">Forgot password?</a>
                   </div>
