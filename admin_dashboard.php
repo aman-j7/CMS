79e2fa99369668b78d7585136296fe7185f39858
HarshKandpal <harshkandpal172000@gmail.com>
2021-12-30 23:20:08 +0530

    update

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -12,17 +12,16 @@ if( $row['password']=="CMS@123"   ){
 ?>
 <html>
     <head>
-    <script>
-    #header {
-  background: url(https://images.unsplash.com/photo-1415795854641-f4a487a0fdc8?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80) center center / cover no-repeat ;
-}
-</script>
+    <title>
+      Admin Dashboard
+    </title>
+    <link rel="stylesheet" href="CSS/admin.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
     <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <img src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp" alt="E-learning"/>
+    <img src="https://seeklogo.com/images/G/graduated-online-education-logo-2327B5F5C0-seeklogo.com.png" width="100" height="60" alt="E-learning"/>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -56,45 +55,40 @@ if( $row['password']=="CMS@123"   ){
     </div>
   </div>
 </nav>
-<nav class="bg-dark navbar-dark">
-  <div class="container">
-    <a href="" class="navbar-brand"><i class="fas fa-tree mr-2"></i>Forest</a>
-  </div>
-  </nav>
    <section id="header" class="jumbotron text-center">
-     <h1 class="display-3">FOREST</h1>
-     <p class="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+     <h1 class="display-3">Welcome</h1>
 </section>
   
 <section id="gallery">
-  <div class="container-fluid ">
-    <div class="row">
-    <div class="col-lg-4 mb-4 ">
-    <a href="">
+  <div class="container">
+    <div class="row ">
+    <div class="col-lg-4 mb-4 mt-4 ">
+    <a href="https://practice.geeksforgeeks.org/courses/Java-Foundation?source=google&medium=cpc&device=c&keyword=gfg&matchtype=b&campaignid=12547146199&adgroup=119586647019&gclid=CjwKCAiAzrWOBhBjEiwAq85QZ2kLyQH7PA7rGSeXCyLWc8PuTWgeU2w6MZA-_E3qRumdRIMCBSJhDRoCzAgQAvD_BwE" style="color:black">
     <div class="card" >
-      <img src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top">
+      <img src="https://news.miami.edu/life/_assets/images/images-stories/2019/08/faculty-new-year-940x529.jpg" alt="" class="card-img-top">
       <div class="card-body">
-        <h5 class="card-title">Manage Faculty</h5>
+        <h5 class="card-title text-center">Manage Faculty</h5>
        </a>
       </div>
      </div>
     </div>
-  <div class="col-lg-4 mb-4 ">
+  <div class="col-lg-4 mb-4 mt-4">
+  <a href="" style="color:black"> 
   <div class="card">
-      <img src="https://images.unsplash.com/photo-1516214104703-d870798883c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top">
+      <img src="https://www.designmantic.com/blog/wp-content/uploads/2020/07/Graphic-Design-Courses-718x300.jpg" alt="" class="card-img-top">
       <div class="card-body">
-        <h5 class="card-title">Manage Student</h5>
-       <a href="" class="btn btn-outline-success btn-sm">Read More</a>
-      
+        <h5 class="card-title text-center">Manage Student</h5>
+  </a>
       </div>
       </div>
     </div>
-    <div class="col-lg-4 mb-4 ">
+    <div class="col-lg-4 mb-4 mt-4">
+    <a href="" style="color:black">
     <div class="card">
-      <img src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top">
+      <img src="https://media1.thehungryjpeg.com/thumbs2/800_121015_63396a0e1974444fdcdfc91bc487db4074c67f9b_happy-people-reading-books-in-garden.jpg" alt="" class="card-img-top">
       <div class="card-body">
-        <h5 class="card-title">Manage Courses</h5>
-       <a href="" class="btn btn-outline-success btn-sm">Read More</a>
+        <h5 class="card-title text-center">Manage Courses</h5>
+    </a>
       </div>
      </div>
     </div>
@@ -103,38 +97,38 @@ if( $row['password']=="CMS@123"   ){
 
   <div class="row">
     <div class="col-lg-4 mt-4">
+    <a href="" style="color:black">
     <div class="card">
-      <img src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top">
+      <img src="https://www.easywork.asia/wp-content/uploads/2021/07/feature-attendance-report.svg" alt="" class="card-img-top" style="background-color:skyblue">
       <div class="card-body">
-        <h5 class="card-title">Monitor Attendance</h5>
-       <a href="" class="btn btn-outline-success btn-sm">Read More</a>
+        <h5 class="card-title text-center">Monitor Attendance</h5>
+    </a>
       </div>
      </div>
     </div>
   <div class="col-lg-4 mt-4">
+  <a href="" style="color:black">
   <div class="card">
-      <img src="https://images.unsplash.com/photo-1516214104703-d870798883c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top">
+      <img src="https://icon-library.com/images/result-icon/result-icon-26.jpg" alt="" class="card-img-top">
       <div class="card-body">
-        <h5 class="card-title">Monitor Result</h5>
-       <a href="" class="btn btn-outline-success btn-sm">Read More</a>
+        <h5 class="card-title text-center">Monitor Result</h5>
+  </a>
       </div>
       </div>
     </div>
     <div class="col-lg-4 mt-4">
+    <a href="" style="color:black">
     <div class="card">
-      <img src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top">
+      <img src="https://trackrover.com/wp-content/uploads/2019/07/Automatic-Attendance-and-Employee-Efficiency-Monitoring-Solution.jpg" alt="" class="card-img-top">
       <div class="card-body">
-        <h5 class="card-title">Settings</h5>
-       <a href="" class="btn btn-outline-success btn-sm">Read More</a>
-
+        <h5 class="card-title text-center">Settings</h5>
+    </a>
       </div>
      </div>
     </div>
   </div>
 </div>
 </section>
-
-
   </div>
 </div>
    </body>
