9bec1e1f97c2617eb972077dcf33eb7ec5e8afb2
NoobDeveloperAS <ashutoshsingh.kv@gmail.com>
2021-12-30 16:23:09 +0530

    login update

--- login.php
+++ login.php
@@ -14,17 +14,22 @@ if(isset($_POST["submit"])){
             header("Location:student_dashboard.php");
     }
     else {
-        echo "login failed";
+      echo '<script>alert("Invalid Password")</script>';
     }
 } 
-    
 ?>
+
+
 <html>
+
 <head>
+
 <link rel="stylesheet" href="CSS/cms.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>E-Learning</title>
+
 </head>
+
 <body>
 
 <section class="h-100 gradient-form" style="background-color: #eee;">
@@ -36,13 +41,12 @@ if(isset($_POST["submit"])){
             <div class="col-lg-6">
               <div class="card-body p-md-5 mx-md-4">
 
-                <div class="text-center">
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp" style="width: 185px;" alt="logo">
-                  <h4 class="mt-1 mb-5 pb-1">E-Learing Portal</h4>
+                <div class="text-center mb-4">
+                  <img src="https://seeklogo.com/images/G/graduated-online-education-logo-2327B5F5C0-seeklogo.com.png" style="width: 185px;" alt="logo">
                 </div>
 
                 <form method="POST" action="login.php">
-                  <p>Please login to your account</p>
+                  <p><strong>Please login to your account</strong></p>
 
                   <div class="form-outline mb-4">
                     <input type="integer" name="id" required id="login1" class="form-control" placeholder="Registration Number"/>
@@ -53,7 +57,7 @@ if(isset($_POST["submit"])){
                   </div>
 
                   <div class="text-center pt-1 mb-5 pb-1">
-                    <input class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit" name="submit" value="Log In"/>
+                    <input class="btn btn-primary btn-sm gradient-custom-2 mb-3" type="submit" name="submit" value="Log In"/>
                     <br>
                     <a class="text-muted" href="forget.php">Forgot password?</a>
                   </div>
@@ -64,8 +68,8 @@ if(isset($_POST["submit"])){
             </div>
             <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
               <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                <h4 class="mb-4">We are more than just a company</h4>
-                <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <h4 class="mb-4 text-center">Next Gen Learning</h4>
+                <p class="small mb-0"> </p>
               </div>
             </div>
           </div>
@@ -76,4 +80,5 @@ if(isset($_POST["submit"])){
 </section>
 
 </body>
+
 </html>
