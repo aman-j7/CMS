f97b5f3f98b688c4e474a5c157f54ae7429f5b91
NoobDeveloperAS <ashutoshsingh.kv@gmail.com>
2021-12-30 16:23:41 +0530

    bootstrap addition

--- forget.php
+++ forget.php
@@ -66,21 +66,60 @@ if(isset($_POST["submit1"])){
 }
 ?>
 <html>
+
+<head>
+
+<link rel="stylesheet" href="CSS/cms.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>E-Learning</title>
+
+</head>
+
 <body>
+
 <?php if($flag): ?>
-<h2>Please Enter Your Registration No</h2>
-<form method="POST" action="forget.php">
-<table  align="center"  cellspacing=" 25">
-    <tr>
-        <th>Registration No</th>
-        <td><input type="integer" name="reg" required></td>
-    </tr>
-    <tr>
-        <td></td>
-        <td><input id="submit" type="submit"  value="Submit" name="submit1"></td>
-    </tr>
-</table>
-</form>
+
+<section class="h-100 gradient-form" style="background-color: #eee;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-xl-10">
+        <div class="card rounded-3 text-black">
+          <div class="row g-0">
+            <div class="col-lg-6">
+              <div class="card-body p-md-5 mx-md-4">
+
+                <div class="text-center mb-4">
+                  <img src="https://seeklogo.com/images/G/graduated-online-education-logo-2327B5F5C0-seeklogo.com.png" style="width: 185px;" alt="logo">
+                </div>
+
+                <form method="POST" action="forget.php">
+                  <p><strong>Enter Your Registration Number</strong></p>
+
+                  <div class="form-outline mb-4">
+                    <input type="integer" name="reg" required id="login1" class="form-control" placeholder="Registration Number"/>
+                  </div>
+
+                  <div class="text-center pt-1 mb-5 pb-1">
+                    <input class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit" name="submit" value="Submit"/>
+                  </div>
+
+                </form>
+
+              </div>
+            </div>
+            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+              <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                <h4 class="mb-4 text-center">Next Gen Learning</h4>
+                <p class="small mb-0"> </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 <?php elseif(!$flag): ?>
 <h2>Please Enter OTP</h2>
 <form method="POST" action="forget.php">
