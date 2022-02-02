f2d6ce6e7e7bc5158f3592af424d9fb8a31c73f3
NoobDeveloperAS <ashutoshsingh.kv@gmail.com>
2022-02-03 01:01:07 +0530

    login css update

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 $f = 0;
 include "includes/config.php";
-$flag=0;
+$flag = 0;
 if (isset($_POST["submit"])) {
   $e = $_POST["id"];
   $p = $_POST["password"];
@@ -10,10 +10,9 @@ if (isset($_POST["submit"])) {
   if ($row) {
     $_SESSION['user_id'] = $e;
     $_SESSION['type'] = $row['role'];
-    if ($p== "CMS@123") {
-          $flag=1;
-    }
-    elseif ($row['role'] == "teacher")
+    if ($p == "CMS@123") {
+      $flag = 1;
+    } elseif ($row['role'] == "teacher")
       header("Location:Teacher/teacher_dashboard.php");
     elseif ($row['role'] == "admin")
       header("Location:Admin/admin_dashboard.php");
@@ -30,7 +29,6 @@ if (isset($_POST["submit"])) {
 <html>
 
 <head>
-
   <link rel="stylesheet" href="CSS/login.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
@@ -39,31 +37,31 @@ if (isset($_POST["submit"])) {
 
 <body>
   <?php
-   if($flag)
-    echo'<script>
+  if ($flag)
+    echo '<script>
     swal("Login Successfull!","update your default password", "success", {
       button: "OK",
     }).then(function() {
     window.location = "Password/change_password.php";
 });
-  </script>';?>
-  <section class="h-100 gradient-form" style="background-color: #eee;">
-    <div class="container py-5 h-100">
+  </script>'; ?>
+  <section class="h-100 gradient-form">
+    <div class="container h-100">
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-xl-10">
           <div class="card rounded-3 text-black">
             <div class="row g-0">
               <div class="col-lg-6">
                 <div class="card-body p-md-5 mx-md-4">
-                  <div class="text-center mb-4">
-                    <img src="https://seeklogo.com/images/G/graduated-online-education-logo-2327B5F5C0-seeklogo.com.png" style="width: 185px;" alt="logo">
+                  <div class="text-center">
+                    <img src="images/logo.png" id="logo" style="width: 180px;" alt="logo">
                   </div>
                   <p id="error"></p>
                   <form method="POST" action="login.php" autocomplete="off">
                     <p><strong>Please login to your account</strong></p>
 
                     <div class="form-outline mb-4">
-                      <input type="integer" name="id" required  class="form-control" placeholder="Registration Number" />
+                      <input type="integer" name="id" required class="form-control" placeholder="Registration Number" />
                     </div>
 
                     <div class="form-outline mb-4">
@@ -71,7 +69,8 @@ if (isset($_POST["submit"])) {
                       <p id="error_pass"></p>
                     </div>
                     <div class="form-outline mb-4 form-check form-switch">
-                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault "onclick="pass_toggle()"><h6>Show password</h6>
+                      <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault " onclick="pass_toggle()">
+                      <h6>Show password</h6>
                     </div>
                     <div class="text-center pt-1 mb-5 pb-1">
                       <input class="btn btn-primary btn-sm gradient-custom-2 mb-3" type="submit" name="submit" value="Log In" />
@@ -83,10 +82,18 @@ if (isset($_POST["submit"])) {
 
                 </div>
               </div>
-              <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                  <h4 class="mb-4 text-center">Next Gen Learning</h4>
-                  <p class="small mb-0"> </p>
+              <div class="col-lg-6 d-flex align-items-center">
+                <div class="mySlides fade">
+                  <img src="images/quote1.jpg" alt="logo" style="width: 100%;">
+                </div>
+                <div class="mySlides fade">
+                  <img src="images/quote2.png" alt="logo" style="width: 100%;">
+                </div>
+                <div class="mySlides fade">
+                  <img src="images/quote3.jpg" alt="logo" style="width: 100%;">
+                </div>
+                <div class="mySlides fade">
+                  <img src="images/quote4.jpg" alt="logo" style="width: 100%;">
                 </div>
               </div>
             </div>
@@ -101,15 +108,35 @@ if (isset($_POST["submit"])) {
         '<div class="alert alert-danger" role="alert">Invalid login, please try again</div>';
 
     }
-    function pass_toggle(){
+
+    function pass_toggle() {
       var x = document.getElementById("login1");
       if (x.type === "password") {
         x.type = "text";
-      } 
-      else {
+      } else {
         x.type = "password";
       }
 
+    }
+
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {
+        slideIndex = 1
+      }
+      
+      slides[slideIndex - 1].style.display = "block";
+      
+      setTimeout(showSlides, 4000); // Change image every 2 seconds
     }
   </script>
   <?php
