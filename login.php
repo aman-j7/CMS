9bf61039c7f52a2dd23f1f2b2214e6458c966244
NoobDeveloperAS <ashutoshsingh.kv@gmail.com>
2022-02-03 14:59:40 +0530

    sweet alert update

--- login.php
+++ login.php
@@ -31,7 +31,7 @@ if (isset($_POST["submit"])) {
 <head>
   <link rel="stylesheet" href="CSS/login.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.16/dist/sweetalert2.all.min.js"></script>
   <script type="text/javascript" src="js/login.js"></script>
   <title>E-Learning</title>
 </head>
@@ -39,13 +39,16 @@ if (isset($_POST["submit"])) {
 <body>
   <?php
   if ($flag)
-    echo '<script>
-    swal("Login Successfull!","update your default password", "success", {
-      button: "OK",
+    echo "<script>
+    Swal.fire({
+      icon: 'warning',
+      title: 'Login Successfull!',
+      text: 'Update your default password',
+      timer: 10000
     }).then(function() {
-    window.location = "Password/change_password.php";
+    window.location = 'Password/change_password.php';
 });
-  </script>'; ?>
+  </script>"; ?>
   <section class="h-100 gradient-form">
     <div class="container h-100">
       <div class="row d-flex justify-content-center align-items-center h-100">
