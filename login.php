f2d68dfef1d3a707f93f046555fdd898b07dc37d
HarshKandpal <harshkandpal172000@gmail.com>
2022-02-05 00:42:56 +0530

    update

--- login.php
+++ login.php
@@ -33,7 +33,8 @@ if (isset($_POST["submit"])) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.16/dist/sweetalert2.all.min.js"></script>
   <script type="text/javascript" src="js/login.js"></script>
-  <title>E-Learning</title>
+  <title>Next Gen Learning</title>
+  <link rel = "icon" href = "images/favicon.ico" type = "image/x-icon">
 </head>
 
 <body>
