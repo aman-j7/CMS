6a5ced3315c90d270c34b25bf786615d0d94e30e
aman-j7 <463amanj@gmail.com>
2022-05-01 16:35:56 +0530

    dark mode

--- js/sidebar.js
+++ js/sidebar.js
@@ -9,16 +9,15 @@ toggle.addEventListener("click", () => {
   sidebar.classList.toggle("close");
 })
 
-modeSwitch.addEventListener("click", () => {
+modeSwitch.addEventListener("click", setTheme );
+function setTheme(){
   body.classList.toggle("dark");
-
   if (body.classList.contains("dark")) {
     modeText.innerText = "Light mode";
   } else {
     modeText.innerText = "Dark mode";
-
   }
-});
+}
 
 function updateUserStatus(){
   jQuery.ajax({
